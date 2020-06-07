e58f8f0da4ec4be91a7b4867c46a381a47d3109e
williamwjd <william.jose.wjd@gmail.com>
2020-06-07 09:38:11 -0300

    trabalhando com o mail-composer

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -5,6 +5,7 @@ import { Feather as Icon, FontAwesome } from '@expo/vector-icons'
 import { useNavigation, useRoute } from '@react-navigation/native'
 import { RectButton } from 'react-native-gesture-handler'
 import api from '../../services/api'
+import * as MailComposer from 'expo-mail-composer';
 
 interface Params{
   point_id:number
@@ -44,6 +45,13 @@ const Detail = () => {
     navigation.goBack()
   }
 
+  function handleComposeMail(){
+    MailComposer.composeAsync({
+      subject: 'Interesse na coleta de resíduos',
+      recipients: [data.point.email]
+    })
+  }
+
   if(!data.point){
     return null
   }
@@ -77,7 +85,7 @@ const Detail = () => {
           <Text style={styles.buttonText}>Whatsapp</Text>
         </RectButton>
 
-        <RectButton style={styles.button} onPress={()=>{}}>
+        <RectButton style={styles.button} onPress={handleComposeMail}>
           <Icon name="mail" size={20} color="#fff" />
           <Text style={styles.buttonText}>E-mail</Text>
         </RectButton>
