897c8e02d84aa66ea5797e4ed290e0d44fc8281f
williamwjd <william.jose.wjd@gmail.com>
2020-06-09 11:16:39 -0300

    validando dados no backend com o celebrate

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import routes from './routes'
 import { resolve } from 'path'
+import { errors } from 'celebrate'
 
 const app = express()
 
@@ -11,6 +12,8 @@ app.use(routes)
 
 //ROTA PARA ACESSO DE ARQUIVOS ESTÁTICOS
 app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')))
+
+app.use(errors())
 
 app.listen(3333, ()=>{
     console.log('Servidor Online')
