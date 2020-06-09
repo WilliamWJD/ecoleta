0f5f8e359738a7711d54780fd5fb2baa25a20950
williamwjd <william.jose.wjd@gmail.com>
2020-06-09 09:13:17 -0300

    corrigindo o metodo de salvar um ponto de coleta

--- backend/src/controllers/PointsController.ts
+++ backend/src/controllers/PointsController.ts
@@ -52,7 +52,7 @@ class PointsController {
         const trx = await knex.transaction()
 
         const point = {
-            image: 'https://images.unsplash.com/photo-1556767576-5ec41e3239ea?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
+            image: req.file.filename,
             name,
             email,
             whatsapp,
@@ -66,12 +66,15 @@ class PointsController {
 
         const point_id = ids[0]
 
-        const pointItems = items.map((item_id: Number) => {
-            return {
-                item_id,
-                point_id
-            }
-        })
+        const pointItems = items
+            .split(',')
+            .map((item: string) => Number(item.trim()))
+            .map((item_id: number) => {
+                return {
+                    item_id,
+                    point_id
+                }
+            })
 
         //RELACIONAMENTO COM A TABELA DE ITEMS
         await trx('point_items').insert(pointItems)
