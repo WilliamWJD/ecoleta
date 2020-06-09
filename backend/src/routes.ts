af5ff1f54977da0fff88418ca26c8f4666888eb6
williamwjd <william.jose.wjd@gmail.com>
2020-06-09 09:00:11 -0300

    configurando o multer

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,13 +1,16 @@
 import { Router } from 'express'
+import multer from 'multer'
+import multerConfig from './config/multer'
 
 import PointController from './controllers/PointsController'
 import ItemsController from './controllers/ItemsController'
 
 const routes = Router()
+const upload = multer(multerConfig)
 
 routes.get('/items', ItemsController.index)
 
-routes.post('/points', PointController.create)
+routes.post('/points', upload.single('image') ,PointController.create)
 routes.get('/points/:id', PointController.show)
 routes.get('/points', PointController.index)
 
